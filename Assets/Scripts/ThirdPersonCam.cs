ae6fb5ae3a79608329e9e8e3297208b49b2a1823
n1k1ta97 <qopkawrka@gmail.com>
2025-09-26 17:42:06 +0700

    Add other mode for camera (combat)

--- Assets/Scripts/ThirdPersonCam.cs
+++ Assets/Scripts/ThirdPersonCam.cs
@@ -1,14 +1,26 @@
+using Cinemachine;
 using UnityEngine;
 
 public class ThirdPersonCam : MonoBehaviour
 {
     [Header("References")]
     public Transform lookDirection;
+    public Transform combatLookAt;
     public Transform player;
     public Transform playerObj;
     public Rigidbody rb;
 
     public float rotationSpeed;
+
+    [Header("Cam Settings")]
+    public CameraStyle camStyle;
+    public GameObject basicCam;
+    public GameObject combatCam;
+
+    public enum CameraStyle
+    {
+        Basic, Combat
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,20 +32,45 @@ public class ThirdPersonCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.F3))
+        {
+            if (basicCam.activeInHierarchy)
+            {
+                basicCam.SetActive(false);
+                combatCam.SetActive(true);
+            }
+            else if (combatCam.activeInHierarchy)
+            {
+                combatCam.SetActive(false);
+                basicCam.SetActive(true);
+            }
+            else
+            {
+                basicCam.SetActive(true);
+            }
+        }
+
         // Rotate look direction of player
         Vector3 viewDirection = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         lookDirection.forward = viewDirection.normalized;
 
-        // Rotate player object
-        float hInput = Input.GetAxisRaw("Horizontal");
-        float vInput = Input.GetAxisRaw("Vertical");
-        Vector3 inputDirection = lookDirection.forward * vInput + lookDirection.right * hInput;
-
-        if (inputDirection != Vector3.zero)
+        if (camStyle == CameraStyle.Basic)
         {
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
-            //Quaternion targetRotation = Quaternion.LookRotation(new Vector3(inputDirection.x, 0, inputDirection.z));
-            //playerObj.rotation = Quaternion.Slerp(playerObj.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+            // Rotate player object
+            float hInput = Input.GetAxisRaw("Horizontal");
+            float vInput = Input.GetAxisRaw("Vertical");
+            Vector3 inputDirection = lookDirection.forward * vInput + lookDirection.right * hInput;
+
+            if (inputDirection != Vector3.zero)
+            {
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
+            }
+        }
+        else if (camStyle == CameraStyle.Combat)
+        {
+            Vector3 combatViewDirection = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+            lookDirection.forward = combatViewDirection.normalized;
+            playerObj.forward = combatViewDirection.normalized;
         }
     }
 }
